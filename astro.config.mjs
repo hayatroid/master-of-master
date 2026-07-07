792c7ab19923c29ec5316b16625fd759be69e33c
hayatroid <hayatroid@gmail.com>
2026-07-08 00:15:21 +0900

    content

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,26 @@ import { SITE } from "./src/site.config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import { unified } from "@astrojs/markdown-remark";
+
+import rehypeExternalLinks from "rehype-external-links";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.url,
   integrations: [solidJs()],
+
+  markdown: {
+    processor: unified({
+      rehypePlugins: [
+        [
+          rehypeExternalLinks,
+          { target: "_blank", rel: ["noopener", "noreferrer"] },
+        ],
+      ],
+    }),
+    shikiConfig: { theme: "github-light" },
+  },
 
   vite: {
     plugins: [tailwindcss()],
